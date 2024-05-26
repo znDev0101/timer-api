8d6302f5ca32fe82e4cf9359f64a9a26f72dc2c0
zulfa nurhuda <zulfaanurhuda28@gmail.com>
2024-05-26 22:51:27 +0700

    fixing bugs

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ const io = socketIo(server, {
   },
 })
 
-app.use(cors())
+const corsOptions = {
+  origin: "https://deadline-timer.vercel.app",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 
 mongoose.connect(
